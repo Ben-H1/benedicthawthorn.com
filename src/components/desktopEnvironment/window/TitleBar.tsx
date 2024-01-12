bfa2ec5351397873ce2c8e5d38290bfed2e641c4
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-12 20:51:27 +0000

    Truncate window title

--- src/components/desktopEnvironment/window/TitleBar.tsx
+++ src/components/desktopEnvironment/window/TitleBar.tsx
@@ -13,9 +13,9 @@ type TitleBarProps = {
 const TitleBar = ({ name, isActive, buttonSet, buttonFunctions, dragHandleClassName }: TitleBarProps) => {
     return (
         <div className='bg-white h-6 shrink-0 flex items-center px-2 space-x-2 border-b-black border-b'>
-            <div className={cn('flex flex-1 select-none space-x-2', dragHandleClassName)}>
+            <div className={cn('flex truncate flex-1 select-none space-x-2', dragHandleClassName)}>
                 <TitleBarLines isActive={isActive} />
-                <div>{name}</div>
+                <div className='truncate'>{name}</div>
                 <TitleBarLines isActive={isActive} />
             </div>
             <TitleBarButtons buttonSet={buttonSet} buttonFunctions={buttonFunctions} />
