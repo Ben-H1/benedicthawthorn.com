8ecd924bbe559de6fa99c48321e47627fe2ff998
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-13 21:01:09 +0000

    Add className pass through for links

--- src/components/text/Link.tsx
+++ src/components/text/Link.tsx
@@ -1,20 +1,22 @@
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { cn } from '@util/css';
 import { ReactNode } from 'react';
 
 type LinkProps = {
     link: string;
     openInNewTab?: boolean;
+    className?: string;
     children: ReactNode;
 };
 
-const Link = ({ link, openInNewTab, children }: LinkProps) => {
+const Link = ({ link, openInNewTab, className, children }: LinkProps) => {
     return (
         <a
             href={link}
             target={openInNewTab ? '_blank' : undefined}
             rel='noreferrer noopener'
-            className='text-indigo-800 hover:underline'
+            className={cn('text-indigo-800 hover:underline', className)}
         >
             {children}
             {openInNewTab && (
