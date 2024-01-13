5655686107a4d053d6dbc2983c168c5f399282c2
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-13 21:09:04 +0000

    Add header components

---
+++ src/components/text/H3.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@util/css';
+import { ReactNode } from 'react';
+
+type H3Props = {
+    className?: string;
+    children: ReactNode;
+};
+
+const H3 = ({ className, children }: H3Props) => {
+    return (
+        <h3 className={cn('font-appleGaramond text-2xl', className)} >
+            {children}
+        </h3>
+    );
+};
+
+export default H3;
