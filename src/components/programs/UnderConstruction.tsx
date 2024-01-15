9a431a4e8d1774bde6124f7fc578e381ccf5cce3
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-15 20:34:52 +0000

    Set width and height on under construction icon

--- src/components/programs/UnderConstruction.tsx
+++ src/components/programs/UnderConstruction.tsx
@@ -3,7 +3,10 @@ import H2 from '@components/text/H2';
 const UnderConstruction = () => {
     return (
         <div className='flex flex-col h-full items-center justify-center p-8 space-y-2'>
-            <img src='src\assets\images\under-construction.png'></img>
+            <img
+                src='src\assets\images\under-construction.png'
+                className='h-8 w-8'
+            />
             <H2>Under Construction</H2>
         </div>
     );
