fb8010e0507b151bc8368053199a05a64c8abe1a
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-16 02:10:24 +0000

    Update title bar lines

--- src/components/desktopEnvironment/window/TitleBarLines.tsx
+++ src/components/desktopEnvironment/window/TitleBarLines.tsx
@@ -9,8 +9,10 @@ const TitleBarLines = ({ isActive }: TitleBarLinesProps) => {
         <div className={cn('flex-grow flex items-center min-w-4', !isActive && 'invisible')}>
             <table className='w-full'>
                 <tbody>
-                    <tr><td className='border-b border-t border-collapse border-black h-1'></td></tr>
-                    <tr><td className='border-b border-t border-collapse border-black h-1'></td></tr>
+                    <tr><td className='border-b border-t border-collapse border-black h-1/2'></td></tr>
+                    <tr><td className='border-b border-t border-collapse border-black h-1/2'></td></tr>
+                    <tr><td className='border-b border-t border-collapse border-black h-1/2'></td></tr>
+                    <tr><td className='border-b border-t border-collapse border-black h-1/2'></td></tr>
                 </tbody>
             </table>
         </div>
