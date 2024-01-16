f4fffc263fa6c032846efe22c91ffd1c4650624a
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-16 00:43:22 +0000

    Move common class to parent

--- src/components/programs/Welcome.tsx
+++ src/components/programs/Welcome.tsx
@@ -3,12 +3,12 @@ import H1 from '@components/text/H1';
 
 const Welcome = () => {
     return (
-        <div className='flex flex-col space-y-2 items-center'>
+        <div className='flex flex-col space-y-2 items-center text-center'>
             <H1>Welcome!</H1>
-            <Body className='text-center'>Hi there, welcome to my website!</Body>
-            <Body className='text-center'>Here, you can find information about my software projects, music, work experience, and more.</Body>
-            <Body className='text-center'>To navigate around the site, you can treat it like a normal desktop environment. Feel free to double-click on a desktop icon to open a program, drag windows around, and explore!</Body>
-            <Body className='text-center'>For the best experience, please use a Chromium-based browser.</Body>
+            <Body>Hi there, welcome to my website!</Body>
+            <Body>Here, you can find information about my software projects, music, work experience, and more.</Body>
+            <Body>To navigate around the site, you can treat it like a normal desktop environment. Feel free to double-click on a desktop icon to open a program, drag windows around, and explore!</Body>
+            <Body>For the best experience, please use a Chromium-based browser.</Body>
         </div>
     );
 };
