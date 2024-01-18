98501f9f2eb7b6eed942256049ea90e73997aab0
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-18 02:13:29 +0000

    Add temporary text on click functionality for button

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -1,21 +1,36 @@
 import { cn } from '@util/css';
-import { MouseEventHandler, ReactNode } from 'react';
+import { MouseEventHandler, ReactNode, useState } from 'react';
 
 type ButtonProps = {
     onClick?: MouseEventHandler<HTMLButtonElement>;
+    tempChildren?: ReactNode;
+    tempDelayMs?: number;
     className?: string;
     children: ReactNode;
     [prop: string]: unknown;
 };
 
-const Button = ({ onClick, className, children, ...props }: ButtonProps) => {
+const Button = ({ onClick, tempChildren, tempDelayMs = 1000, className, children, ...props }: ButtonProps) => {
+    const [buttonChildren, setButtonChildren] = useState(children);
+
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (...args) => {
+        if (onClick) {
+            onClick(...args);
+        }
+
+        if (tempChildren) {
+            setButtonChildren(tempChildren);
+            setTimeout(() => setButtonChildren(children), tempDelayMs);
+        }
+    };
+
     return (
         <button
-            onClick={onClick}
+            onClick={handleClick}
             className={cn('border-black border px-2 py-1 rounded-md active:bg-gray-300', className)}
             {...props}
         >
-            {children}
+            {buttonChildren}
         </button>
     );
 };
