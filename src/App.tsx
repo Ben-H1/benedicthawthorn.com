2d107c355a253caea0f70190530eb99a98469941
Ben-H1 <benedicthawthorn@gmail.com>
2024-01-29 21:43:06 +0000

    Try to fix fade height

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import DesktopEnvironment from '@components/desktopEnvironment/DesktopEnvironmen
 const App = () => {
     return (
         <>
-            <div className='absolute inset-0 pointer-events-none z-[9999999999] backdrop-filter animate-fadeOut'></div>
+            <div className='absolute inset-0 pointer-events-none z-[9999999999] backdrop-filter animate-fadeOut h-full'></div>
             <div className='absolute text-transparent select-none pointer-events-none'>
                 <p className='font-chicago'>.</p>
                 <p className='font-appleGaramond'>.</p>
