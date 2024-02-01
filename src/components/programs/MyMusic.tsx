49e197bbba9f6b0ea609a62c9468a6e4c5605e1b
Ben-H1 <benedicthawthorn@gmail.com>
2024-02-01 22:58:15 +0000

    Add second song

--- src/components/programs/MyMusic.tsx
+++ src/components/programs/MyMusic.tsx
@@ -6,12 +6,22 @@ const MyMusic = () => {
             audioPath: 'music/ukulele/Just the Two of Us.mp3',
             title: 'Just the Two of Us (Ukulele Cover)',
             artist: 'Benedict Hawthorn'
+        },
+        {
+            audioPath: 'music/ukulele/Fragile.mp3',
+            title: 'Fragile (Ukulele Cover)',
+            artist: 'Benedict Hawthorn'
         }
     ];
 
     return (
-        <div>
-            <Mp3Player track={tracks[0]} />
+        <div className='flex flex-col space-y-4'>
+            {tracks.map((track, i) => (
+                <>
+                    <Mp3Player track={track} />
+                    {i < tracks.length - 1 && <hr />}
+                </>
+            ))}
         </div>
     );
 };
