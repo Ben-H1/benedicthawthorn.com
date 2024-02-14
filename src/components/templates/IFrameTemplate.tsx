e59d73ac66def0abaa4effd46e915eb97fd964d0
Ben-H1 <benedicthawthorn@gmail.com>
2024-02-14 14:31:16 +0000

    Fix iframe window padding on scroll

--- src/components/templates/IFrameTemplate.tsx
+++ src/components/templates/IFrameTemplate.tsx
@@ -7,7 +7,7 @@ type IFrameTemplateProps = {
 
 const IFrameTemplate = ({ src }: IFrameTemplateProps) => {
     return (
-        <div className='w-full flex flex-col h-full'>
+        <div className='w-full flex flex-col min-h-full'>
             <iframe className='w-full flex-1' src={src} />
             <div className='mt-4 flex justify-end'>
                 <Text.Body><Link href={src} openInNewTab>Open in new tab</Link></Text.Body>
